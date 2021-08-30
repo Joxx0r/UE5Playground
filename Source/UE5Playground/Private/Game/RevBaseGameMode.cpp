e05bcea7bb7220b4ff8f0a3cd6f70f8b373e53e9
Joxx0r <Joxxor@gmail.com>
2021-08-30 15:02:04 +0200

    Reparented classes to new names

---
+++ Source/UE5Playground/Private/Game/RevBaseGameMode.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Game/RevBaseGameMode.h"
+#include "Core/RevPlayerCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ARevBaseGameMode::ARevBaseGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+}
---
+++ Source/UE5Playground/Public/Game/RevBaseGameMode.h
@@ -0,0 +1,17 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RevBaseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARevBaseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARevBaseGameMode();
+};
+
+
